4e4699c9b0070af5ab5ed348de4a1857d70dc541
Bharathvuppalapatti18 <149554359+Bharathvuppalapatti18@users.noreply.github.com>
2025-06-02 20:29:28 +0530

    Delete src directory

--- src/app/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export const Footer = () => (
-    <footer className="bg-dark text-white text-center py-4 mt-5">
-      <div className="container">
-        <p className="mb-0">&copy; 2025 PikFusion. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-  
